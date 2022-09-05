c858fb2917dc2b2a48d6e95cede8b5fa3cbc4bda
Larshabi <lekanlasabi@gmail.com>
2022-09-05 23:53:56 +0100

    users

--- authentication/models.py
+++ authentication/models.py
@@ -38,7 +38,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True, max_length=255)
     phone = PhoneNumberField(null=False)
     is_admin = models.BooleanField(default=False)
-    is_active=models.BooleanField(default=False)
+    is_active=models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser= models.BooleanField(default=False)
     
